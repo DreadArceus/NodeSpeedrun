e4c707b28ece6ff48cf7798cbd55fe37f0504a0c
Mayank Talwar <dreadarceus@gmail.com>
2020-12-07 21:23:14 +0530

    feat: new user reporting

--- js/client.js
+++ js/client.js
@@ -4,17 +4,17 @@ const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
 const messageContainer = document.querySelector(".container");
 
-// const append = (message, position) => {
-//   const messageElement = document.createElement("div");
-//   messageElement.innerText = message;
-//   messageElement.classList.add("message");
-//   messageElement.classList.add(position);
-//   messsageContainer.append(messageElement);
-// };
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+};
 
 const name = prompt("Enter you name to join");
 socket.emit("new-user-joined", name);
 
-// socket.on("user-joined", (name) => {
-//   append(name + " just joined", "left");
-// });
+socket.on("user-joined", (name) => {
+  append(`${name} just joined`, "left");
+});
